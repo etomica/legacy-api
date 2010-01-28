e9460a6bd08755d266358890661599c36c6f2c2d
Mike Sellers <msellers@buffalo.edu>
2010-01-28 15:38:23 +0000

    commented out print statemtents.  changed output formatting.  production ready.

--- etomica.sandbox/etomica/lammpslib/LMSDProcessorOrderN.java
+++ etomica.sandbox/etomica/lammpslib/LMSDProcessorOrderN.java
@@ -95,18 +95,18 @@ public class LMSDProcessorOrderN {
             if(newDeltaTmax>numBlocks){
             	buffnum = (int)Math.floor(Math.log10(numBlocks))+1;
             }
-            System.out.println("DeltaTMax is now "+(buffnum*10)+".");
+            System.out.println("DeltaTMax is now "+(Math.pow(10, buffnum)+1)+".");
             return;
     	}
     	
     	if(newDeltaTmax>numBlocks){
     		System.out.println("DeltaTMax must be smaller than the total number of configuration blocks.  Fixing...");
     		buffnum = (int)Math.floor(Math.log10(numBlocks))+1;
-    		System.out.println("DeltaTMax is now "+(Math.pow(10, buffnum))+".");
+    		System.out.println("DeltaTMax is now "+(Math.pow(10, buffnum)+1)+".");
     		return;
     	}
     	buffnum = (int)Math.floor(Math.log10(newDeltaTmax))+1;
-    	System.out.println("DeltaTMax is now "+(Math.pow(10, buffnum))+1+".");
+    	System.out.println("DeltaTMax is now "+(Math.pow(10, buffnum)+1)+".");
     }
 
     public void fillArrays(){
@@ -132,7 +132,7 @@ public class LMSDProcessorOrderN {
         	
         	//LOOP OVER CONFIGURATION BLOCKS
         	for (int i=0; i<numBlocks; i++){
-                System.out.println("Calculating for configuration block "+i);
+                //System.out.println("Calculating for configuration block "+i);
                 
                 //Advance past Atom Number and "Atoms" lines.
                 buffReader.readLine();
@@ -157,16 +157,15 @@ public class LMSDProcessorOrderN {
 	                else {tb *= 10;}
 	            	//set largest deltaT(maxj) in BUFFER
                     int maxj = i/tb;
-                    
-            	    
- //           		System.out.println(i+"  "+b+" "+tb+" "+maxj);
+                    	    
+            		//System.out.println(i+"  "+b+" "+tb+" "+maxj);
             		
             		if (maxj*tb != i) break;
             		//Copy 1detaT into current buffer start
         			for(int a=0; a<numAtoms; a++){
         				coordBlocks[b][0][a].E(coordBlocks[0][0][a]);
         			}
-  //      			System.out.println(">Copied current position info into dt 0 for buffer "+b);
+        			//System.out.println(">Copied current position info into dt 0 for buffer "+b);
 	            }
 	            
 	            //LOOP OVER BUFFERS TO PROCESS DATA
@@ -179,7 +178,7 @@ public class LMSDProcessorOrderN {
                     if (maxj*tb != i) break;
             	    if (maxj>9) maxj = 9;   
             	    
-//            		System.out.println(i+"  "+b+" "+tb+" "+maxj);
+            		//System.out.println(i+"  "+b+" "+tb+" "+maxj);
 	            
         			//PROCESS DATA FROM BUFFERS FOR: +XYZ MSD, +MSD PerAtom, +Z-profile
         			//loop over deltaT's
@@ -194,7 +193,7 @@ public class LMSDProcessorOrderN {
             				}
             			}
         				
-//            			if (b==1) System.out.println(">>Calculating MSD of dt "+j+" for buffer "+b);
+            			//if (b==1) System.out.println(">>Calculating MSD of dt "+j+" for buffer "+b);
         				
             			//MAIN MSD CALC
         				for(int a=0; a<numAtoms; a++){
@@ -208,7 +207,7 @@ public class LMSDProcessorOrderN {
         				
         				//PER-ATOM DISPLACEMENT IN XY (deltaT=500, must have at least 1000 configurations to use) 
         				if(b==2){
-        					if(j==4){
+        					if(j==2){
             					for(int a=0; a<numAtoms; a++){
             						coordVectorAtom.Ev1Mv2(coordBlocks[b][j][a],coordBlocks[b][0][a]);
             						//remove Z-component
@@ -232,7 +231,7 @@ public class LMSDProcessorOrderN {
         			//BOOKKEEPING STEP, FREE UP FIRST ROW IF WE COPIED CONFIGURATION DATA
         			IVectorMutable[] lastBlock = coordBlocks[b][9];
                 	for(int dt=9; dt>0; dt--){
-//                		System.out.println(">>>Shifting blocks of dt "+dt+" for buffer "+b);
+                		//System.out.println(">>>Shifting blocks of dt "+dt+" for buffer "+b);
                 	    coordBlocks[b][dt] = coordBlocks[b][dt-1];
                 	}
                 	coordBlocks[b][0] = lastBlock;
@@ -281,7 +280,6 @@ public class LMSDProcessorOrderN {
             fileWriter.write("DeltaTMax: "+Math.pow(10,buffnum)+"\n");     
             
             for(int k=0;k<numSpecies;k++){
-            	fileWriter.write("___SPECIES "+(k+1)+" Time dependent data for X,Y,Z_______________________\n");
             	int bt=0;
             	for(int b=0;b<buffnum;b++){
             		if(b==0){bt=1;}
@@ -289,24 +287,24 @@ public class LMSDProcessorOrderN {
             		for (int j=1; j<10; j++){
 	            		fileWriter.write((bt*j)+" "+RsquaredXYZ[k][b][j][0]+" "+RsquaredXYZ[k][b][j][1]+" "+RsquaredXYZ[k][b][j][2]+" "+(RsquaredXYZ[k][b][j][0]+RsquaredXYZ[k][b][j][1]+RsquaredXYZ[k][b][j][2])+"\n");
             		}
+            		
             	}
             	fileWriter.write("\n");
             }
             
-            fileWriter.write("___Density Profile_______________________\n");
-            fileWriter.write("+Range is "+histz.getXRange().minimum()+" to "+histz.getXRange().maximum()+".\n");
-            fileWriter.write("+Number of bins is "+histz.getNBins()+".\n");
+            fileWriter.write("rho\n");
+            fileWriter.write("+range: "+histz.getXRange().minimum()+" - "+histz.getXRange().maximum()+"\n");
+            fileWriter.write("+bins: "+histz.getNBins()+"\n");
             for(int i=0;i<histz.getHistogram().length;i++){
             	fileWriter.write(histz.getHistogram()[i]+"\n");	
             }
             
-            fileWriter.write("___Z-plane Diffusion Data_______________________\n");
-            fileWriter.write("+Range is "+histdelt2.getXRange().minimum()+" to "+histdelt2.getXRange().maximum()+".\n");
-            fileWriter.write("+Number of bins is "+histdelt2.getNBins()+".\n");
+            fileWriter.write("zplane-diff\n");
+            fileWriter.write("+range: "+histdelt2.getXRange().minimum()+" to "+histdelt2.getXRange().maximum()+".\n");
+            fileWriter.write("+bins: "+histdelt2.getNBins()+".\n");
             double D;
             for(int i=0;i<histdelt2.getHistogram().length;i++){
-            	D=(histdelt2.getHistogram()[i]-histdelt1.getHistogram()[i])/(4*500);
-            	fileWriter.write(D+"\n");
+            	fileWriter.write(((histdelt2.getHistogram()[i]-histdelt1.getHistogram()[i])/(700))+"\n");
             }
             
             fileWriter.close();
